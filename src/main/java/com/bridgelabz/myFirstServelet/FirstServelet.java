9489c7b6133fd59a523f3952288fdb11866a695d
tomthomas1 <tomthomas8688@gmail.com>
2022-05-03 17:04:36 +0530

    UC1 : Created the first servelet using tom cat

---
+++ src/main/java/com/bridgelabz/myFirstServelet/FirstServelet.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.myFirstServelet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class FirstServelet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Welcome To My First Servelet Program</h2>");
+        out.close();
+    }
+}
